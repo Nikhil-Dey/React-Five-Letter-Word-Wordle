1d214735526f13adf4c5773efe55391049d2b2b4
Nikhil Dey <deynikhil963@gmail.com>
2024-10-20 22:34:16 +0530

    added multiple chances for guessing the letter

--- src/App.jsx
+++ src/App.jsx
@@ -9,34 +9,36 @@ function Square({value, feedback}){
   )
 }
 
-function Letter(){
+function Letter({active, gameStatus, handleGameStatus, index}){
   const [letter, setLetter] = useState("");
-  const [index, setIndex] = useState(0);
   const [validated, setValidated] = useState(false);
 
   useEffect(() => {
     function handleSetLetter(key){
-      if (index === 5) return;
+      if (letter.length === 5) return;
       setLetter(letter + key.toUpperCase());
-      setIndex(index + 1);
     }
 
     function handleRemovingLetter(){
-      if (index === 0) return;
+      if (letter.length === 0) return;
       setLetter(letter.slice(0, -1));
-      setIndex(index - 1);
     }
 
     function handleValidateString(){
-      if (index !== 5){
+      if (letter.length !== 5){
         console.log('Please fill all the boxes in a row');
-      }else{
+      }else {
+        handleGameStatus(letter === targetValue)
         console.log('validated');
         setValidated(true);
       }
     }
   
     function handleKeyEvent(e){
+      console.log(e.key, active, gameStatus, index);
+      if(!active || gameStatus){
+        return;
+      }
       const key = e.key;
   
       if (/^[a-zA-Z]$/.test(key)){
@@ -51,7 +53,7 @@ function Letter(){
     }
     window.addEventListener('keydown', handleKeyEvent);
     return () => window.removeEventListener('keydown', handleKeyEvent);
-  }, [index]);
+  }, [letter, active, gameStatus]);
   
   function feedback(i){
     if(!validated){
@@ -67,7 +69,7 @@ function Letter(){
   }
 
   return (
-    <div className="row">
+    <div className="row" disabled={!active}>
       {[0, 1, 2, 3, 4].map(i => (
         <Square key={i} value={letter[i] || ''} feedback={feedback(i)} />
       ))}
@@ -75,4 +77,33 @@ function Letter(){
   )
 }
 
-export default Letter;
+function Wordle(){
+  const [currentRow, setCurrentRow] = useState([true, false, false, false, false, false]);
+  const [gameStatus, setGameStatus] = useState(false);
+  const [index, setIndex] = useState(0);
+
+  function handleGameStatus(status){
+    if(status){
+      setCurrentRow(Array(9).fill(false));
+      setGameStatus(status);
+      setIndex(6);
+    }else{
+      if (index === 6) return;
+      setCurrentRow(currentRow.map((row, i) => i === index + 1 ? true : false));
+      setIndex(index + 1);
+    }
+  }
+
+  return (
+    <div>
+    <Letter key={0} active={currentRow[0]} gameStatus={gameStatus} handleGameStatus={handleGameStatus} index={0}/>
+    <Letter key={1} active={currentRow[1]} gameStatus={gameStatus} handleGameStatus={handleGameStatus} index={1}/>
+    <Letter key={2} active={currentRow[2]} gameStatus={gameStatus} handleGameStatus={handleGameStatus} index={2}/>
+    <Letter key={3} active={currentRow[3]} gameStatus={gameStatus} handleGameStatus={handleGameStatus} index={3}/>
+    <Letter key={4} active={currentRow[4]} gameStatus={gameStatus} handleGameStatus={handleGameStatus} index={4}/>
+    <Letter key={5} active={currentRow[5]} gameStatus={gameStatus} handleGameStatus={handleGameStatus} index={5}/>
+    </div>
+  )
+}
+
+export default Wordle;
